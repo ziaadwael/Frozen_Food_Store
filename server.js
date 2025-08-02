1898fa41ed9cf7238b74e5c8406943a498b45a46
Ziad Wael <ziadwaelelfeky@gmail.com>
2025-08-02 22:12:38 +0300

    Fix Deployment Problems

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'public')));
+
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
